4fd40d9693399eb57f13561a56b5926bbbfc6ae1
mdsoaibakh-eng <mdsoaibakh>
2026-01-09 21:15:29 +0530

    Add fixed header navigation and About Us page with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import AboutUs from "./pages/AboutUs"; 
+import AboutUs from "./pages/AboutUs";
 import Dashboard from "./pages/Dashboard";
 import CoursePage from "./pages/CoursePage";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/course/:id" element={<CoursePage />} />
-      </Routes>
+      {/* HEADER */}
+      <header className="header">
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+      </header>
+
+      {/* PAGE CONTENT */}
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/course/:id" element={<CoursePage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
